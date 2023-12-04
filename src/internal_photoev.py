11c579f5dbb23408ce82d6ace5490c20ce170ca4
benoit tabone <benoit.tabone@ias.u-psud.fr>
2023-12-04 15:22:54 +0100

    Correct Sigma_dot for Owen and Picogna option + implement Komaki option

--- src/internal_photoev.py
+++ src/internal_photoev.py
@@ -13,9 +13,50 @@ from scipy import integrate
 from .constants import *
 from .disc import AccretionDisc
 
+
+def Sigma_dot_Picogna(R=np.array([0.]),a=0,b=0,c=0,d=0,e=0,f=0,g=0):
+    # Goal: provide Sigma_dot for the ansatz of Picogna+2021
+    # Input: R in au and a,b,c,... coefficients of the fits
+    # Output: Sigma_dot, in au-2 since this has to be multiplied by MW(LX,MW) (eq. 5,6,7 Picogna+2021)
+    ln10  = np.log(10.)
+    lnR   = np.log(R)
+    logR  = np.log10(R)
+
+    Sigma_dot = ln10*(6*a*lnR**5/(R*ln10**6) +  5*b*lnR**4/(R*ln10**5)  +  \
+        4*c*lnR**3/(R*ln10**4)  +  3*d*lnR**2/(R*ln10**3) +  \
+        2*e*lnR/(R*ln10**2) +  f/(R*ln10) ) / \
+        (2.*np.pi*R) \
+        *10**(a*logR**6+b*logR**5+c*logR**4+d*logR**3+e*logR**2+f*logR+g)
+    Sigma_dot[Sigma_dot<0] = 0.
+
+    return Sigma_dot 
+
+def MW_corr_picogna(LX_soft=1e29):
+    # Goal: Eq. 6 in Ercolano+2021 to compute the MW form LX (if picogna option)
+    a = -1.947e17
+    b = -1.572e-4 # don't understand the relevance of this parameter: it has negligible contribution to MW_corr_picogna
+    c = -2.866e-1
+    d = -6.694
+    #
+    MW = a*np.exp( (np.log(np.log10(LX_soft) - b))**2/c ) + d
+    MW = 10**MW 
+
+    return MW
+
+def MW_picogna(Mstar=1.,LX=1e29):
+    # Goal: compile Picogna's reciepe to compute the total mass-loss rate as function of Mstar and LX
+    # see Ercolano+2021
+    MW_mean       = 3.93e-8*(Mstar) #mass loss-rate corresponding to the median for a give stellar mass
+    L_X_soft      = 10**(0.95*np.log10(LX)+1.19) # luminosity in the 0.1-
+    LX_mean       = 10**(1.54* np.log10(Mstar) + 30.31)
+    L_X_soft_mean = 10**(0.95*np.log10(LX_mean)+  1.19) 
+    MW = MW_mean*MW_corr_picogna(LX_soft=L_X_soft)/MW_corr_picogna(LX_soft=L_X_soft_mean)
+    return MW
+
+
 class internal_photoev():
     """
-    Compute the photoevaporative mass loss term following Owen et al. (2012)
+    Compute the photoevaporative mass loss term following the option set for the mass-loss rate profile
     """
 
     def __init__(self, disc, mdot_photoev_profile):
@@ -35,7 +76,7 @@ class internal_photoev():
                 self.mdot_X = 6.25e-9 * (disc._star.M)**(-0.068)*(disc._L_x)**(1.14)
 
             self.norm_X = 1.299931298429752e-07  # Normalization factor obtained via numerical integration - \int 2 \pi x \Sigma(x) dx * au**2/Msun
-            self.x = 0.85*(self._radius)*(disc._star.M)**(-1.)
+            self.x      = 0.85*(self._radius)*(disc._star.M)**(-1.)
             self.index_null_photoevap = np.searchsorted(self.x, 2)
 
             a1 = 0.15138
@@ -65,45 +106,70 @@ class internal_photoev():
 
             self._Sigmadot_Owen = self._Sigmadot_Owen_unnorm/self.norm_X*self.mdot_X*(disc._star.M)**(-2)   # Normalizing
             self._Sigmadot_Owen[self._Sigmadot_Owen < 0] = 0.                                         # Setting to zero every negative value - safety 
+            self._Sigmadot_Owen = self._Sigmadot_Owen/(2.*np.pi) # code units !
 
             self.hole = False
 
         elif mdot_photoev_profile=="picogna":
-            self.mdot_X = disc._mdot_photoev
+            if disc._mdot_photoev != 0:
+                self.mdot_X = disc._mdot_photoev
+            else:
+                self.mdot_X = MW_picogna(Mstar=disc._star.M,LX=disc._L_x)
 
             mstars=np.array([0.1,0.3,0.5,1.0])
-            a_mstar=np.array([-3.8337,-1.3206,-1.2320,-0.6344])
-            b_mstar=np.array([22.91,13.0475,10.8505,6.3587])
-            c_mstar=np.array([-55.1282,-53.6990,-38.6939,-26.1445])
-            d_mstar=np.array([67.8919,117.6027,71.2489,56.4477])
-            e_mstar=np.array([-45.0138,-144.3769,-71.4279,-67.7403,])
-            f_mstar=np.array([16.2977,94.7854,37.8707,43.9212])
-            g_mstar=np.array([-3.5426,-26.7363,-9.3508,-13.2316])
+            a=np.array([ -3.8337,  -1.3206,  -1.2320, -0.6344])
+            b=np.array([ 22.91,    13.0475,  10.8505,  6.3587])
+            c=np.array([-55.1282, -53.6990, -38.6939,-26.1445])
+            d=np.array([ 67.8919, 117.6027,  71.2489, 56.4477])
+            e=np.array([-45.0138,-144.3769, -71.4279,-67.7403])
+            f=np.array([ 16.2977,  94.7854,  37.8707, 43.9212])
+            g=np.array([ -3.5426, -26.7363,  -9.3508,-13.2316])
 
             mstar = disc._star.M
-            a=np.interp(mstar,mstars,a_mstar)
-            b=np.interp(mstar,mstars,b_mstar)
-            c=np.interp(mstar,mstars,c_mstar)
-            d=np.interp(mstar,mstars,d_mstar)
-            e=np.interp(mstar,mstars,e_mstar)
-            f=np.interp(mstar,mstars,f_mstar)
-            g=np.interp(mstar,mstars,g_mstar)
 
             x=self._radius
-            lnx=np.log(x)
-            ln10=np.log(10.)
-            self._Sigmadot_Picogna = ( 6.*a*lnx**5./(x*ln10**6.) + 5.*b*lnx**4./(x*ln10**5.)+4.*c*lnx**3./(x*ln10**4.) + \
-                                3.*d*lnx**2./(x*ln10**3.) + 2.*e*lnx/(x *ln10**2.) + f/(x*ln10) ) / x
-            
-            self._Sigmadot_Picogna *= 10.**(a*lnx**6.+b*lnx**5.+c*lnx**4.+d*lnx**3.+e*lnx**2.+f*lnx+g)
+
+
+            ind  = np.searchsorted(mstars,mstar,side='left')
+            if (ind == 0):             # mstar smaller than 0.1Msun => no extrapolations !
+                Sigma_dot       = Sigma_dot_Picogna(R=x,a=a[0],b=b[0],c=c[0],d=d[0],e=e[0],f=f[0],g=g[0])
+            elif (ind == len(mstars)): # mstar larger than 1Msun => no exptrapolations !
+                Sigma_dot       = Sigma_dot_Picogna(R=x,a=a[-1],b=b[-1],c=c[-1],d=d[-1],e=e[-1],f=f[-1],g=g[-1])
+            else:                      # interpolation
+                Sigma_dot_left  = Sigma_dot_Picogna(R=x,a=a[ind-1], b=b[ind-1], c=c[ind-1],d=d[ind-1], e=e[ind-1],f=f[ind-1], g=g[ind-1])
+                Sigma_dot_right = Sigma_dot_Picogna(R=x,a=a[ind],   b=b[ind],   c=c[ind],  d=d[ind],   e=e[ind],  f=f[ind],   g=g[ind])
+                Sigma_dot       = Sigma_dot_left + (mstar - mstars[ind-1]) * (Sigma_dot_right-Sigma_dot_left)/(mstars[ind] - mstars[ind-1] )
+
+            self._Sigmadot_Picogna =  3.93e-8*mstar*Msun*Sigma_dot/AU**2/yr    # in g s-1 cm-2 => scalings are not necessary but just to check that we recover MW in Picogna+2021 fig. 6
+            norm = np.trapz(2*np.pi*x*AU*self._Sigmadot_Picogna,x*AU)
+            self._Sigmadot_Picogna *= self.mdot_X*Msun/yr/norm                 # in g s-1 cm-2 with correct resulting MW
+            self._Sigmadot_Picogna *= yr/(2.*np.pi)                            # in code units
+ 
+
+        elif mdot_photoev_profile=="komaki":
+            if disc._mdot_photoev != 0:
+                self.mdot_X = disc._mdot_photoev
+            else:
+                raise NotImplementedError("The requested photo-evaporation profile type " + mdot_photoev_profile + " can't use LX to compute mass-loss rate. Please set lx=0 and prescribe a value for mdot_photoev")
+
+            mstar = disc._star.M
+            rad   = self._radius
+            x     = self._radius/(8.87*mstar) # radius normalised by the grav radius rg (Komaki+21)
+            logx = np.log10(x)
+
+            self._Sigmadot_Komaki = 0.693*logx**5 -0.95*logx**4 -0.038*logx**3 +0.678*logx**2 -1.67*logx -12.6
+            self._Sigmadot_Komaki = 10**self._Sigmadot_Komaki * np.exp(-(x/17.)**2) # B. Tabone: I added a tapper to avoid divergence at large distance
+
 
             #normalise profile
-            norm = np.trapz(2*np.pi*x*AU*self._Sigmadot_Picogna,x*AU)
-            self._Sigmadot_Picogna *= self.mdot_X*Msun/yr/norm
+            norm = np.trapz(2*np.pi*rad*AU*self._Sigmadot_Komaki,rad*AU)
+            self._Sigmadot_Komaki *= self.mdot_X*Msun/yr/norm # in g cm-2 s-1
 
+            self._Sigmadot_Komaki *= yr/(2.*np.pi)            # in code units
 
 
         elif mdot_photoev_profile=="alexander":
+            print("You selected alexander mass-loss rate: this option is not validated. Please test this option (units for example)!")
             alpha_b  = 2.6e-13  # recombination coefficient of atomic hydrogen
             self.sigcrit=1e-5
             c_s=10 * 1000 * 100. #10 km/s in cgs units
@@ -146,6 +212,7 @@ class internal_photoev():
         Determine the \dot \Sigma term to add to the evolution equation.
         Check whether the gap is already opened: if not, use the standard prescription implemented in the initialization. If yes, use the switch prescription - which depends on the radius of the hole.
         '''
+        self._flag_dispersion = False
         if self.mdot_photoev_profile=="owen":
             sigma_threshold = 1e22                                                  # Density threshold under which the hole is considered to be open
             self._flag_dispersion = False
@@ -177,7 +244,8 @@ class internal_photoev():
                     
                     after_hole = self.y >0
 
-                    if not any(after_hole):
+                    #if not any(after_hole):
+                    if np.sum(after_hole)<2:
                         self._flag_dispersion = True
                         print('The hole is too large now - I will stop the evolution')
                         return 0, True
@@ -208,6 +276,9 @@ class internal_photoev():
             
         elif self.mdot_photoev_profile=="picogna":
             return self._Sigmadot_Picogna, False
+
+        elif self.mdot_photoev_profile=="komaki":
+            return self._Sigmadot_Komaki, False
         
         elif self.mdot_photoev_profile=="alexander":
             self._flag_dispersion = False
